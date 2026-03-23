b460323521051b9bce97aefed74427c0426b4842
Chechekhins <vadimchex97@gmail.com>
2026-03-23 16:46:43 +0300

    Add files via upload

--- docs/tutorials/notebooks/scEve/scEve_predict_R.R
+++ docs/tutorials/notebooks/scEve/scEve_predict_R.R
@@ -15,16 +15,20 @@ use_condaenv('scparadise')
 sc <- import("scanpy", convert = FALSE)
 scp <- import("scparadise", convert = FALSE)
 
+## Download dataset from 10x Genomics
 # Specify the URL of the file you want to download
-url <- "https://cf.10xgenomics.com/samples/cell-exp/6.1.0/10k_PBMC_3p_nextgem_Chromium_Controller/10k_PBMC_3p_nextgem_Chromium_Controller_filtered_feature_bc_matrix.h5"
+url <- "https://cf.10xgenomics.com/samples/cell-exp/6.1.0/10k_PBMC_3p_nextgem_Chromium_X/10k_PBMC_3p_nextgem_Chromium_X_filtered_feature_bc_matrix.h5"
 # Specify the file name and location where you want to save the file on your computer
-file_name <- "10k_PBMC_3p_nextgem_Chromium_Controller_filtered_feature_bc_matrix.h5"
+file_name <- "10k_PBMC_3p_nextgem_Chromium_X_filtered_feature_bc_matrix.h5"
 file_path <- ""
 # Call the download.file() function, passing in the URL and file name/location as arguments
+# Or just paste the url to a browser and download it directly
 download.file(url, paste(file_path, file_name, sep = ""), mode = "wb")
 
 # Load dataset using file name/location
 object <- Read10X_h5(paste(file_path, file_name, sep = ""))
+# Or read your Seurat object:
+# object <- readRDS('object.rds')
 
 ## Preprocessing 
 # Find doublets using scDblFinder
@@ -48,19 +52,20 @@ VlnPlot(
   group.by = 'scDblFinder.class'
 )
 
-## Subset low quality cells
+## Remove low quality cells
 object <-
   subset(object, subset = 
            nFeature_RNA > 200 &
            nFeature_RNA < 5000 & 
            nCount_RNA < 20000 & 
            percent.mt < 15)
+## Remove doublets
 object <-
   subset(object, scDblFinder.class == 'singlet')
 
 # Data normalization, PCA
 DefaultAssay(object) <- 'RNA'
-object <- NormalizeData(object) |> 
+object <- SCTransform(object, vst.flavor = 'v2', vars.to.regress = "percent.mt") |> 
   FindVariableFeatures() |> 
   ScaleData() |> 
   RunPCA() |> 
@@ -81,10 +86,6 @@ object[["RNA_convert"]] <- NULL
 sc$pp$normalize_total(adata, target_sum = NULL) #, target_sum = FALSE)
 # Logarithmize the data
 sc$pp$log1p(adata)
-# Set the .raw attribute of the AnnData object 
-# to the normalized and logarithmized gene expression 
-# for later use by scparadise
-adata$raw <- adata
 
 # Download dataframe with available models 
 df <- scp$sceve$available_models()
@@ -92,10 +93,10 @@ View(df)
 
 ## scEve 
 # Download scEve model
-scp$sceve$download_model(model_name = 'Human_PBMC', save_path = '')
+scp$sceve$download_model(model_name = 'Human_PBMC_3p', save_path = '')
 
 # Impute surface proteins using scEve model
-adata_adt_imp <- scp$sceve$predict(adata, path_model = 'Human_PBMC_scEve', return_mdata = FALSE)
+adata_adt_imp <- scp$sceve$predict(adata, path_model = 'Human_PBMC_3p_scEve', return_mdata = FALSE)
 
 ## scAdam (you can skip this step if cells are already annotated)
 # Download scAdam model
@@ -115,7 +116,7 @@ adt_imp@Dimnames[[2]] <- py_to_r(adata_adt_imp$obs_names$tolist())
 rm(adata_adt_imp)
 
 # Create ADT assay in Seurat object
-adt_imp <- CreateAssay5Object(data = adt_imp)
+adt_imp <- CreateAssay5Object(counts = adt_imp, data = adt_imp)
 object@assays$ADT <- adt_imp
 object@assays[["ADT"]]@key <- 'adt_'
 
@@ -123,7 +124,8 @@ object@assays[["ADT"]]@key <- 'adt_'
 DefaultAssay(object) <- 'ADT'
 VariableFeatures(object) <- rownames(object[["ADT"]])
 # scEve predicts normalized data, so we skip normalization step
-object <- ScaleData(object) |> 
+object <- FindVariableFeatures(object,  nfeatures = 100) |> 
+  ScaleData() |> 
   RunPCA(reduction.name = 'apca')
 
 ElbowPlot(object, ndims = 50, reduction = 'pca')
@@ -132,8 +134,7 @@ ElbowPlot(object, ndims = 50, reduction = 'apca')
 # Construct weighted nearest neighbor graph
 object <- FindMultiModalNeighbors(
   object, reduction.list = list("pca", "apca"), 
-  k.nn = 15,
-  dims.list = list(1:15, 1:20)
+  dims.list = list(1:15, 1:15)
 )
 
 # UMAP dimensional reduction using WNN nn 
@@ -147,125 +148,145 @@ object <-
 
 ## Visualize predictions on UMAP
 # Celltype_l1
-p1 <- DimPlot(object,
-              group.by = c('pred_celltype_l1'),
-              pt.size = 2,
-              reduction = 'wnn.umap', # WNN UMAP
-              label = T,
-              label.size = 5, 
-              repel = T) + NoLegend()
-p2 <- DimPlot(object,
-              group.by = c('pred_celltype_l1'),
-              pt.size = 2,
-              reduction = 'umap', # UMAP calculated using only gene expression data
-              label = T,
-              label.size = 5, 
-              repel = T) + NoLegend()
+p1 <- DimPlot(
+  object,
+  group.by = c('pred_celltype_l1'),
+  reduction = 'wnn.umap', # WNN UMAP
+  label = T,
+  repel = T
+) + NoLegend() + ggplot2::ggtitle("WNN UMAP")
+p2 <- DimPlot(
+  object,
+  group.by = c('pred_celltype_l1'),
+  reduction = 'umap',  # UMAP calculated using only gene expression data
+  label = T,
+  repel = T
+) + NoLegend() + ggplot2::ggtitle("RNA UMAP")
 p1+p2
 
 # Celltype_l2
-p1 <- DimPlot(object,
-              group.by = c('pred_celltype_l2'),
-              pt.size = 2,
-              reduction = 'wnn.umap', # WNN UMAP
-              label = T,
-              label.size = 4, 
-              repel = T) + NoLegend()
-p2 <- DimPlot(object,
-              group.by = c('pred_celltype_l2'),
-              pt.size = 2,
-              reduction = 'umap', # UMAP calculated using only gene expression data
-              label = T,
-              label.size = 4, 
-              repel = T) + NoLegend()
+p1 <- DimPlot(
+  object,
+  group.by = c('pred_celltype_l2'),
+  reduction = 'wnn.umap', # WNN UMAP
+  label = T,
+  repel = T
+) + NoLegend() + ggplot2::ggtitle("WNN UMAP")
+p2 <- DimPlot(
+  object,
+  group.by = c('pred_celltype_l2'),
+  reduction = 'umap',  # UMAP calculated using only gene expression data
+  label = T,
+  repel = T
+) + NoLegend() + ggplot2::ggtitle("RNA UMAP")
 p1+p2
 
 # Celltype_l3
-p1 <- DimPlot(object,
-              group.by = c('pred_celltype_l3'),
-              pt.size = 2,
-              reduction = 'wnn.umap', # WNN UMAP
-              label = T,
-              label.size = 3, 
-              repel = T) + NoLegend()
-p2 <- DimPlot(object,
-              group.by = c('pred_celltype_l3'),
-              pt.size = 2,
-              reduction = 'umap', # UMAP calculated using only gene expression data
-              label = T,
-              label.size = 3, 
-              repel = T) + NoLegend()
+p1 <- DimPlot(
+  object,
+  group.by = c('pred_celltype_l3'),
+  reduction = 'wnn.umap', # WNN UMAP
+  label = T,
+  repel = T
+) + NoLegend() + ggplot2::ggtitle("WNN UMAP")
+p2 <- DimPlot(
+  object,
+  group.by = c('pred_celltype_l3'),
+  reduction = 'umap',  # UMAP calculated using only gene expression data
+  label = T,
+  repel = T
+) + NoLegend() + ggplot2::ggtitle("RNA UMAP")
 p1+p2
-
 
 ## Check predictions and imputations
 # CD4+ T cells
-FeaturePlot(object,
-            features = c('CD3E', # RNA
-                         'CD4', # RNA
-                         'adt-CD3-2-pred', # ADT
-                         'adt-CD4-1-pred' # ADT
-                         ),
-            order = T,
-            pt.size = 1)
+FeaturePlot(
+  object,
+  features = c('CD3E', # RNA
+               'CD4', # RNA
+               'adt-CD3-2-pred', # ADT
+               'adt-CD4-1-pred' # ADT
+               ),
+  order = T,
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+)
 
 # CD8+ T cells
-FeaturePlot(object,
-            features = c('CD3E', # RNA
-                         'CD8A', # RNA
-                         'adt-CD3-2-pred', # ADT
-                         'adt-CD8a-pred' # ADT
-            ),
-            order = T,
-            pt.size = 1)
+FeaturePlot(
+  object,
+  features = c('CD3E', # RNA
+               'CD8A', # RNA
+               'adt-CD3-2-pred', # ADT
+               'adt-CD8a-pred' # ADT
+  ),
+  order = T,
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+)
 
 # CD14+ Monocytes
-FeaturePlot(object,
-            features = c('CD14', # RNA
-                         'LYZ', # RNA
-                         'adt-CD14-pred', # ADT
-                         'adt-CD11c-pred' # ADT
-                         ),
-            order = T,
-            pt.size = 1)
+FeaturePlot(
+  object,
+  features = c('CD14', # RNA
+               'LYZ', # RNA
+               'adt-CD14-pred', # ADT
+               'adt-CD11c-pred' # ADT
+               ),
+  order = T,
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+)
 
 # CD16+ Monocytes
-FeaturePlot(object,
-            features = c('FCGR3A', # RNA
-                         'MS4A7', # RNA
-                         'adt-CD16-pred', # ADT
-                         'adt-CX3CR1-pred' # ADT
-            ),
-            order = T,
-            pt.size = 1)
+FeaturePlot(
+  object,
+  features = c('FCGR3A', # RNA
+               'MS4A7', # RNA
+               'adt-CD16-pred', # ADT
+               'adt-CX3CR1-pred' # ADT
+  ),
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+  order = T
+)
 
 # B
-FeaturePlot(object,
-            features = c('CD79A', # RNA
-                         'BANK1', # RNA
-                         'adt-CD19-pred', # ADT
-                         'adt-CD20-pred' # ADT
-            ),
-            order = T,
-            pt.size = 1) 
+FeaturePlot(
+  object,
+  features = c('CD79A', # RNA
+               'BANK1', # RNA
+               'adt-CD19-pred', # ADT
+               'adt-CD20-pred' # ADT
+  ),
+  order = T,
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+) 
 
 # NK
-FeaturePlot(object,
-            features = c('KLRF1', # RNA
-                         'FCER1G', # RNA
-                         'adt-CD56-1-pred', # ADT
-                         'adt-CD56-2-pred' # ADT
-            ),
-            order = T,
-            pt.size = 1) 
+FeaturePlot(
+  object,
+  features = c('KLRF1', # RNA
+               'FCER1G', # RNA
+               'adt-CD56-1-pred', # ADT
+               'adt-CD56-2-pred' # ADT
+  ),
+  order = T,
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+) 
 
 # Platelet
-FeaturePlot(object,
-            features = c('PF4', # RNA
-                         'PPBP', # RNA
-                         'adt-CD42b-pred', # ADT
-                         'adt-CD62P-pred' # ADT
-            ),
-            order = T,
-            pt.size = 1)
+FeaturePlot(
+  object,
+  features = c('PF4', # RNA
+               'PPBP', # RNA
+               'adt-CD42b-pred', # ADT
+               'adt-CD62P-pred' # ADT
+  ),
+  order = T,
+  max.cutoff = 'q99',
+  min.cutoff = 'q01',
+)
 
